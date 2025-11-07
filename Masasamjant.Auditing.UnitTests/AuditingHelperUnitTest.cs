65cc7eaa2c3cd74687d0d75d28a27d0167aa3414
Matti Jantti <m_jantti@hotmail.com>
2025-11-07 19:27:54 +0200

    Add auditing helper unit test

---
+++ Masasamjant.Auditing.UnitTests/AuditingHelperUnitTest.cs
@@ -0,0 +1,22 @@
+﻿namespace Masasamjant.Auditing
+{
+    [TestClass]
+    public class AuditingHelperUnitTest : UnitTest
+    {
+        [TestMethod]
+        public void Test_GetAuditingActionType()
+        {
+            foreach (var value in Enum.GetValues<AuditingActionType>())
+            {
+                Assert.AreEqual(value, AuditingHelper.GetAuditingActionType(value.ToString()));
+            }
+
+            Assert.AreEqual(AuditingActionType.Other, AuditingHelper.GetAuditingActionType(""));
+            Assert.AreEqual(AuditingActionType.Other, AuditingHelper.GetAuditingActionType("Custom"));
+            Assert.AreEqual(AuditingActionType.Other, AuditingHelper.GetAuditingActionType("Viewing"));
+            Assert.AreEqual(AuditingActionType.Other, AuditingHelper.GetAuditingActionType("Insert"));
+            Assert.AreEqual(AuditingActionType.Other, AuditingHelper.GetAuditingActionType("Testing"));
+            Assert.AreEqual(AuditingActionType.Other, AuditingHelper.GetAuditingActionType("None"));
+        }
+    }
+}
